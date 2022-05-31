0664b46cfa093d5209f7de5f27ce3bd8e0bc9826
Rahul-Reddy03 <106601188+Rahul-Reddy03@users.noreply.github.com>
2022-05-31 19:02:49 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,4 @@
 import streamlit
 streamlit.title('My parents new healthy diner')
-streamlit.heading('Breakfast Menu)
+streamlit.header('Breakfast Menu')
                   streamlit.body('Omega 3 & blueberry oatmeal')
