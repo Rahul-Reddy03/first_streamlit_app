1d607f2edf0098db931b451e729bb3615c5d47ef
Rahul-Reddy03 <106601188+Rahul-Reddy03@users.noreply.github.com>
2022-06-03 00:29:41 +0530

    Update streamlit_app.py
    added input field for users fruit choice

--- streamlit_app.py
+++ streamlit_app.py
@@ -21,6 +21,9 @@ streamlit.dataframe(fruits_to_show)
 
 streamlit.header('Fruityvice Fruit Advice!')
 
+fruit_choice = streamlit.text("What fruit would you like information about?', 'kiwi')
+streamlit.write("The user entered", fruit_choice)                              
+
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "kiwi")
 
